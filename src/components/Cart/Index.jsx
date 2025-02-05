c07fc2a93fdde57fc9cf85c44a5dcb0086148542
kiryu <13matheusilva@gmail.com>
2025-02-05 17:25:53 -0300

    assets file moved to public folder

--- src/components/Cart/Index.jsx
+++ src/components/Cart/Index.jsx
@@ -1,46 +1,67 @@
-import { useContext } from "react"
-import { CartContext } from "../../context/CartContext"
-import { nanoid } from "nanoid"
-import CartItems from "../CartItems"
-import carbonNeutral from "../../assets/images/icon-carbon-neutral.svg"
-import emptyCart from "../../assets/images/illustration-empty-cart.svg"
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import { nanoid } from "nanoid";
+import CartItems from "../CartItems";
 
 export default function Cart() {
-    const { cartItems, totalPrice, cartItemsCount, setConfirmation } = useContext(CartContext)
+  const { cartItems, totalPrice, cartItemsCount, setConfirmation } =
+    useContext(CartContext);
 
-    const cartItemsElements = cartItems.map(product => (
-        <CartItems
-            key={nanoid()}
-            name={product.name}
-            price={product.price}
-            quantity={product.quantity}
-        />
-    ))
+  const cartItemsElements = cartItems.map((product) => (
+    <CartItems
+      key={nanoid()}
+      name={product.name}
+      price={product.price}
+      quantity={product.quantity}
+    />
+  ));
 
-    return (
-        <div className="bg-white p-6 rounded-xl w-80 sm:w-64 sm:text-center">
-            <h2 className="text-orange_red font-bold text-lg mb-4">YOUR CART ({cartItemsCount})</h2>
-            {
-                cartItems.length
-                    ?
-                    <div>
-                        {cartItemsElements}
-                        <div className="flex items-center justify-between mb-4">
-                            <p className="text-sm text-very_dark_rose font-medium">Order Total</p>
-                            <span className="text-2xl font-bold text-very_dark_rose">${totalPrice.toFixed(2)}</span>
-                        </div>
-                        <div className="bg-very_light_rose flex justify-center items-center p-3 rounded mb-4">
-                            <img className="mr-1" src={carbonNeutral} alt="carbon neutral icon" />
-                            <p className="text-xs text-very_dark_rose">This is a <span className="font-semibold">carbon-neutral</span> delivery</p>
-                        </div>
-                        <button onClick={() => setConfirmation(true)} className="bg-orange_red text-white w-full p-3 rounded-full hover:brightness-75 transition">Confirm Order</button>
-                    </div>
-                    :
-                    <div className="flex flex-col items-center">
-                        <img className="mb-4" src={emptyCart} alt="empty cart" />
-                        <p className="text-dark_rose text-sm mb-4 font-medium">Your added items will appear here</p>
-                    </div>
-            }
+  return (
+    <div className="bg-white p-6 rounded-xl w-80 sm:w-64 sm:text-center">
+      <h2 className="text-orange_red font-bold text-lg mb-4">
+        YOUR CART ({cartItemsCount})
+      </h2>
+      {cartItems.length ? (
+        <div>
+          {cartItemsElements}
+          <div className="flex items-center justify-between mb-4">
+            <p className="text-sm text-very_dark_rose font-medium">
+              Order Total
+            </p>
+            <span className="text-2xl font-bold text-very_dark_rose">
+              ${totalPrice.toFixed(2)}
+            </span>
+          </div>
+          <div className="bg-very_light_rose flex justify-center items-center p-3 rounded mb-4">
+            <img
+              className="mr-1"
+              src="/assets/images/icon-carbon-neutral.svg"
+              alt="carbon neutral icon"
+            />
+            <p className="text-xs text-very_dark_rose">
+              This is a <span className="font-semibold">carbon-neutral</span>{" "}
+              delivery
+            </p>
+          </div>
+          <button
+            onClick={() => setConfirmation(true)}
+            className="bg-orange_red text-white w-full p-3 rounded-full hover:brightness-75 transition"
+          >
+            Confirm Order
+          </button>
         </div>
-    )
+      ) : (
+        <div className="flex flex-col items-center">
+          <img
+            className="mb-4"
+            src="/assets/images/illustration-empty-cart.svg"
+            alt="empty cart"
+          />
+          <p className="text-dark_rose text-sm mb-4 font-medium">
+            Your added items will appear here
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/components/CartItems/index.jsx
+++ src/components/CartItems/index.jsx
@@ -3,19 +3,24 @@ import { CartContext } from "../../context/CartContext";
 import { IoCloseCircleOutline } from "react-icons/io5";
 
 export default function CartItems({ name, price, quantity }) {
-    const { removeCartItem } = useContext(CartContext)
+  const { removeCartItem } = useContext(CartContext);
 
-    return (
-        <div className="flex justify-between items-center border-b-[1px] mb-4">
-            <div className="mb-2">
-                <h3 className="text-very_dark_rose font-semibold">{name}</h3>
-                <div className="flex">
-                    <p className="text-orange_red mr-4 font-semibold">{quantity}x</p>
-                    <p className="text-rose mr-2">@${price.toFixed(2)}</p>
-                    <p className="text-dark_rose font-medium">${(price * quantity).toFixed(2)}</p>
-                </div>
-            </div>
-            <IoCloseCircleOutline className="text-xl cursor-pointer text-medium_rose transition hover:text-very_dark_rose" onClick={() => removeCartItem(name)} />
+  return (
+    <div className="flex justify-between items-center border-b-[1px] mb-4">
+      <div className="mb-2">
+        <h3 className="text-very_dark_rose font-semibold">{name}</h3>
+        <div className="flex">
+          <p className="text-orange_red mr-4 font-semibold">{quantity}x</p>
+          <p className="text-rose mr-2">@${price.toFixed(2)}</p>
+          <p className="text-dark_rose font-medium">
+            ${(price * quantity).toFixed(2)}
+          </p>
         </div>
-    )
+      </div>
+      <IoCloseCircleOutline
+        className="text-xl cursor-pointer text-medium_rose transition hover:text-very_dark_rose"
+        onClick={() => removeCartItem(name)}
+      />
+    </div>
+  );
 }
